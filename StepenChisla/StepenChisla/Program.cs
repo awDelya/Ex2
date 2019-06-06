70868cc860b1a305ff7bb2d2f2eaa72e91252e9f
Овчинникова Аделина <deley@mail.ru>
2019-06-06 22:15:23 +0500

    Add function ChisloStepen

--- StepenChisla/StepenChisla/Program.cs
+++ StepenChisla/StepenChisla/Program.cs
@@ -46,10 +46,40 @@ namespace StepenChisla
             } while (!ok);
             return n;
         }
+        private static void ChisloStepen(int number)
+        {
+            bool ok = false;
+            for(int i = 2; i<number; i++)
+            {
+                if(number % i == 0)
+                {
+                    double x = Math.Log10(number) / Math.Log10(i);
+                    if (Math.Pow(i, x) == number)
+                    {
+                        MessGreen("\n YES");
+                        ok = true;
+                        break;
+                    }
+                }
+            }
+            if(!ok)
+                MessRed("\n NO");
+        }
+        private static List<int> chisla = new List<int>();
         static void Main()
         {
-            MessYellow("\n Введите колличество элементов: ");
+            MessYellow("\n Введите колличество элементов (от 1 до 10): ");
             int x = Check(1, 10);
+            for(int i = 0; i<x; i++)
+            {
+                MessYellow("\n Введите " + (i+1) + " число (допусимый диапазон от 1 до 1000000000): ");
+                int n = Check(1, 1000000000);
+                chisla.Add(n);
+            }
+            for (int i = 0; i < x; i++)
+                ChisloStepen(chisla[i]);
+            MessYellow("\n Нажмите любую клавишу чтобы выйти из программы...");
+            Console.ReadKey();
         }
     }
 }
