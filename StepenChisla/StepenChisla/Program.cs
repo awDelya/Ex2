15351a8d1d28adee02ac5be442128f2ac1860ed6
Овчинникова Аделина <deley@mail.ru>
2019-06-06 21:04:47 +0500

    Add function for color messages

--- StepenChisla/StepenChisla/Program.cs
+++ StepenChisla/StepenChisla/Program.cs
@@ -8,6 +8,31 @@ namespace StepenChisla
 {
     class Program
     {
+        private static void MessAsk(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+        private static void MessGreen(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+        private static void MessYellow (string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+        private static void MessRed(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(message);
+            Console.ResetColor();
+        }
         static void Main()
         {
 
